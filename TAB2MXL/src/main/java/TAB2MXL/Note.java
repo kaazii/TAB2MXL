422eb3c6ed3992ef77920210fc14394340685031
Amer Alshoghri <aalshoghri@gmail.com>
2021-02-09 20:30:23 -0500

    Completed guitar functionality for XmlGenerator

--- TAB2MXL/src/main/java/TAB2MXL/Note.java
+++ TAB2MXL/src/main/java/TAB2MXL/Note.java
@@ -17,4 +17,34 @@ public class Note {
 		this.duration = duration;
 		this.octave = octave; 
 	}
+	
+	public Note() {}
+	
+	public void setOctave(int octave) {
+		this.octave = octave;
+	}
+
+	public void setStep(String step) {
+		this.step = step;
+	}
+
+	public void setVoice(int voice) {
+		this.voice = voice;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public void setString(int string) {
+		this.string = string;
+	}
+
+	public void setFret(int fret) {
+		this.fret = fret;
+	}
+	
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
 }
