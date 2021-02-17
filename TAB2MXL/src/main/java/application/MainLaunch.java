f989d7e48f0e34a706656ef2f09d35fc5375f5f9
Doris Zhou <zhoudoris13@gmail.com>
2021-02-17 12:42:48 -0500

    Commit summary:
    - minor fxml update
    
    - mainLaunch.java
    
    - build.gradle

---
+++ TAB2MXL/src/main/java/application/MainLaunch.java
@@ -0,0 +1,10 @@
+package application;
+
+import javafx.application.Application;
+import View.Main2;
+public class MainLaunch {
+
+    public static void main(final String[] args) {
+        Application.launch(Main2.class, args);
+    }
+}
