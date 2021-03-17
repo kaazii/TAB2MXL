a8bfb87f195106f51c56203507cd7c1624071323
Amer Alshoghri <aalshoghri@gmail.com>
2021-03-17 02:29:12 -0400

    added beam class

---
+++ TAB2MXL/src/main/java/TAB2MXL/Beam.java
@@ -0,0 +1,13 @@
+package TAB2MXL;
+
+public class Beam {
+	
+	public int number;
+	public String state;
+	
+	public Beam(int number, String state) {
+		this.number = number;
+		this.state = state;
+	}
+
+}
