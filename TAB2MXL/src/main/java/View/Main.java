f1230fa60f8e7df3f59b4848df120d73e05b22d4
Doris Zhou <zhoudoris13@gmail.com>
2021-03-17 19:07:48 -0400

    Commit Summary:
    fix: Main.java testing out drum
    
    feat: added drum note repository

--- TAB2MXL/src/main/java/View/Main.java
+++ TAB2MXL/src/main/java/View/Main.java
@@ -8,12 +8,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		//new MyFrame();
-		String tab = "BD|x---------------|--------x-------|\n"
+		String tab = "CC|x---------------|--------x-------|\n"
 				+ "HH|--x-x-x-x-x-x-x-|----------------|\n"
 				+ "SD|----o-------o---|oooo------------|\n"
-				+ "HH|----------------|----oo----------|\n"
-				+ "SD|----------------|------oo--------|\n"
-				+ "BD|o-------o-------|o-------o-------|\n";
+				+ "HT|----------------|----oo----------|\n"
+				+ "MT|----------------|------oo--------|\n"
+				+ "BD|o-------o-------|o-------o-------|";
 		List<Measure> measures = StringParserUtilityDrum.stringParse(tab);
 	}
 }
